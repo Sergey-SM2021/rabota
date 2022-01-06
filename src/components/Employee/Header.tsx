4850467d4c41e10a98d9c62822d8c302030a9db9
Sergey <sergey2003.k.96@gmail.com>
2022-01-07 01:34:42 +0300

    кнопка создания резюме

--- src/components/Employee/Header.tsx
+++ src/components/Employee/Header.tsx
@@ -1,16 +1,27 @@
 import styled from "styled-components";
+import {useNavigate} from 'react-router-dom'
 
 const Header = () => {
+    let nav =useNavigate()
     return (<HeaderStyle>
         <div>logo</div>
-        <div>Создать резюме</div>
+        <Button onClick={()=>{nav("/createresume")}}>Создать резюме</Button>
     </HeaderStyle>)
 }
 
 let HeaderStyle = styled.div`
+    margin: 43px 0px;
     color: #fff;
     display: flex;
     justify-content: space-between;
+`
+
+let Button = styled.button`
+    height: 45px;
+    background: none;
+    border-radius: 25px;
+    border: solid 1px #a3daa5;
+    color: #a3daa5;
 `
 
 export default Header
