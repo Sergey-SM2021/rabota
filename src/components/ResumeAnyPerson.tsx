a1f5cd83321fb8f292afa21cccd4ab97f4468cb8
Sergey <sergey2003.k.96@gmail.com>
2022-01-18 16:40:29 +0300

    defaultState

--- src/components/ResumeAnyPerson.tsx
+++ src/components/ResumeAnyPerson.tsx
@@ -1,18 +1,28 @@
-import React from 'react'
 import { useSelector } from 'react-redux'
 import styled from 'styled-components'
 import { getResume } from '../redux/selectors'
 
 const ResumeAnyPerson = () => {
-    let state = useSelector(getResume)
-    return (<Container>
-        {state._id}
-    </Container>
-    )
+  let { name, surename, ...state } = useSelector(getResume)
+  return (<Container>
+    <h6>id : {state._id}</h6>
+    <h2>{name}</h2>
+    <h2>{surename}</h2>
+    <h2>{state.number}</h2>
+    <hr />
+    <h2>{state.data}</h2>
+    Ключевые навыки:
+    { 
+      state.skills.map((el)=>(
+        <h3>*{el}</h3>
+      ))
+    }
+  </Container>
+  )
 }
 
 const Container = styled.div`
-    background: #fff;
+  background: #fff;
 `
 
 export default ResumeAnyPerson
