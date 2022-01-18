e12cc9989d14719013afd64695b9cb09d4a04b52
Sergey <sergey2003.k.96@gmail.com>
2022-01-18 16:48:05 +0300

    Создал Action creater

--- src/redux/myResume.ts
+++ src/redux/myResume.ts
@@ -1,17 +1,39 @@
 import { FormType } from './../types';
 
+enum constants {
+    SETRESUME
+}
+
 let initialState: FormType = {
     _id: "ghbd9hher78w78wee8932",
     data: "Php developer",
     experience: "есть",
-    name: "Алеуксандр",
+    name: "Александр",
     number: "+79168518338",
     skills: ["Java", "Php", "mySQL"],
     surename: "Ткачёв"
 }
 
-const Reducer = (state = initialState, action: any) => {
-    return {...state}
+interface IsetResume {
+    resume: FormType,
+    type: constants.SETRESUME
+}
+
+export const setResume = (resume: FormType): IsetResume => ({
+    resume: resume,
+    type: constants.SETRESUME
+})
+
+type actionType = IsetResume
+
+const Reducer = (state = initialState, action: actionType) => {
+    let stateCopy = { ...state }
+    switch (action.type) {
+        case constants.SETRESUME:
+            return stateCopy
+        default:
+            return stateCopy
+    }
 }
 
 export default Reducer
