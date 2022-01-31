9a9d64df00b6bf296d6b9e0c689940bea950e541
Sergey-SM2021 <84789763+Sergey-SM2021@users.noreply.github.com>
2022-01-31 18:13:30 +0300

    fix: теперь используем scss

--- Frontend/src/components/Createresume/NewResume.tsx
+++ Frontend/src/components/Createresume/NewResume.tsx
@@ -1,5 +1,4 @@
 import { Field, Formik, Form, FieldArray } from 'formik'
-import styled from "styled-components";
 import { useDispatch, useSelector } from "react-redux"
 
 import { SendResume } from "../../redux/newResume";
@@ -8,6 +7,7 @@ import {Button, Spin} from 'antd'
 import {useEffect} from "react"
 import {useLocation, useNavigate} from "react-router-dom"
 import {ClearState} from "../../redux/newResume"
+import scss from './style.module.scss'
 
 const Resume = () => {
     const nav = useNavigate()
@@ -73,31 +73,31 @@ const Resume = () => {
             render={({ values, errors, touched }) => (<>
                 {formIsToggle ? <div><Spin size={"large"}/></div> : err ? <h2>{err}</h2> :
                     <Form>
-                    <Block>
-                        {touched.name && errors.name ? <ErrorMessage>*{errors.name}</ErrorMessage> : <div>Имя</div>}
+                    <div style={{marginTop:10}}>
+                        {touched.name && errors.name ? <div className={scss.error}>*{errors.name}</div> : <div>Имя</div>}
                         <Field name={"name"} />
-                    </Block>
-                    <Block>
-                        {touched.surename && errors.surename ? <ErrorMessage>*{errors.surename}</ErrorMessage> :
+                    </div>
+                    <div style={{marginTop:10}}>
+                        {touched.surename && errors.surename ? <div className={scss.error}>*{errors.surename}</div> :
                             <div>Фамилия</div>}
                         <Field name={"surename"} />
-                    </Block>
-                    <Block>
+                    </div>
+                    <div style={{marginTop:10}}>
                         <div>Номер телефона</div>
                         <Field name={"number"} />
-                    </Block>
-                    <Block>
-                        {touched.data && errors.data ? <ErrorMessage>*{errors.data}</ErrorMessage> :
+                    </div>
+                    <div style={{marginTop:10}}>
+                        {touched.data && errors.data ? <div className={scss.error}>*{errors.data}</div> :
                             <div>Укажите специальность</div>}
                         <Field name={"data"} as="textArea" />
-                    </Block>
-                    <Block>
-                        {touched.experience && errors.experience ? <ErrorMessage>*{errors.experience}</ErrorMessage> :
+                    </div>
+                    <div style={{marginTop:10}}>
+                        {touched.experience && errors.experience ? <div className={scss.error}>*{errors.experience}</div> :
                             <div>Укажите опыт работы</div>}
                         <div><label>есть<Field value={"есть"} name={"experience"} type={"radio"} /></label></div>
                         <label>нет<Field value={"нет"} name={"experience"} type={"radio"} /></label>
-                    </Block>
-                    <Block>
+                    </div>
+                    <div style={{marginTop:10}}>
                         <FieldArray name={'skills'} render={(helpers) => (<div>
                             <div>Навыки
                                 <Button type={"link"} onClick={() => {
@@ -106,32 +106,24 @@ const Resume = () => {
                                 </Button>
                             </div>
                             {
-                                values.skills.map((el, index) => (<Block>
+                                values.skills.map((el, index) => (<div style={{marginTop:10}}>
                                     <Field name={`skills[${index}]`} />
                                     <button type={"button"} onClick={() => {
                                         helpers.remove(index)
                                     }}>Удалить
                                     </button>
-                                </Block>
+                                </div>
                                 ))
                             }
                         </div>)} />
-                    </Block>
-                    <Block>
+                    </div>
+                    <div style={{marginTop:10}}>
                         <button type="submit" disabled={formIsToggle}>Заполнить</button>
-                    </Block>
+                    </div>
                 </Form>}
             </>)}
         />
     </>)
 }
-
-const ErrorMessage = styled.div`
-    color: #ff0000;
-`
-
-const Block = styled.div`
-    margin: 10px 0px;
-`
 
 export default Resume
