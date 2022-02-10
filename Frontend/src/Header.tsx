66f0b516315735b1ba6d4ad8c9162e28e73c85bd
Sergey-SM2021 <84789763+Sergey-SM2021@users.noreply.github.com>
2022-02-10 23:35:11 +0300

    refactor: Поправил навигацию

--- Frontend/src/Header.tsx
+++ Frontend/src/Header.tsx
@@ -8,8 +8,8 @@ const MyHeader:FC = () => {
     return (<Header>
         <div className="logo" />
         <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-            <Menu.Item><NavLink to={"employer"}>Employer</NavLink></Menu.Item>
-            <Menu.Item><NavLink to={"employee"}>Employee</NavLink></Menu.Item>
+            <Menu.Item><NavLink to={"employer"}>Работодатель</NavLink></Menu.Item>
+            <Menu.Item><NavLink to={"employee"}>Соискатель</NavLink></Menu.Item>
             <Menu.Item><NavLink to={"auth"}>Войти</NavLink></Menu.Item>
         </Menu>
     </Header>)
