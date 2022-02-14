3af6edbfdad5e41b74453f6037d61d38ebef1dcb
Sergey-SM2021 <84789763+Sergey-SM2021@users.noreply.github.com>
2022-02-14 12:45:26 +0300

    refactor

--- Frontend/src/components/Employee/Vacance.tsx
+++ Frontend/src/components/Employee/Vacance.tsx
@@ -5,12 +5,13 @@ import { IVacance } from '../../Models/types'
 import Style from './Style.module.sass'
 
 const Vacance: FC<IVacance> = ({ vacance, description, price, skills }) => {
+    const HandleRespondClick = () => { nav("/") }
     const nav = useNavigate()
     return (<Card className={Style.card}>
         <h3>{vacance}</h3>
         <h4 className={Style.price}>{price}тыс. руб</h4>
         <p>{description}</p>
-        <Button onClick={()=>{nav("/")}} type='primary'>Откликнуться</Button>
+        <Button onClick={HandleRespondClick} type='primary'>Откликнуться</Button>
         <Button className={Style.call}>Позвонить</Button>
     </Card>)
 }
