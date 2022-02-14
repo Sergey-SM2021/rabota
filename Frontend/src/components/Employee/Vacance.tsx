3f6237518d60b103b7a325c28db249a9f13d80e1
Sergey-SM2021 <84789763+Sergey-SM2021@users.noreply.github.com>
2022-02-14 12:40:25 +0300

    refactor: fix color & structure vacance

--- Frontend/src/components/Employee/Vacance.tsx
+++ Frontend/src/components/Employee/Vacance.tsx
@@ -2,15 +2,16 @@ import { FC } from 'react'
 import { Button, Card } from 'antd'
 import { useNavigate } from 'react-router-dom'
 import { IVacance } from '../../Models/types'
+import Style from './Style.module.sass'
 
 const Vacance: FC<IVacance> = ({ vacance, description, price, skills }) => {
     const nav = useNavigate()
-    return (<Card style={{ marginTop: 10, marginBottom: 10 }}>
+    return (<Card className={Style.card}>
         <h3>{vacance}</h3>
-        <div>{description}</div>
-        <div>Оплата:{price}</div>
-        <div>{skills.map((el) => (<div>*{el}</div>))}</div>
+        <h4 className={Style.price}>{price}тыс. руб</h4>
+        <p>{description}</p>
         <Button onClick={()=>{nav("/")}} type='primary'>Откликнуться</Button>
+        <Button className={Style.call}>Позвонить</Button>
     </Card>)
 }
 
