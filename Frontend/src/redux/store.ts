ce32d6c269cd7fc0b1e7a1595df2fffac1f0e49a
Sergey-SM2021 <84789763+Sergey-SM2021@users.noreply.github.com>
2022-02-16 21:42:32 +0300

    refactoe store naming

--- Frontend/src/redux/store.ts
+++ Frontend/src/redux/store.ts
@@ -2,19 +2,19 @@ import { applyMiddleware, combineReducers, createStore } from "redux";
 import employee from './reducers/employee'
 import employer from './reducers/employer'
 import thunk from 'redux-thunk'
-import form from './reducers/newResume'
-import myResume from './reducers/myResume'
-import vacance from './reducers/newVacance'
+import CreateResume from './reducers/newResume'
+import someonesResume from './reducers/myResume'
+import CreateVacance from './reducers/newVacance'
 
 const reducers = combineReducers({
     employee,
     employer,
-    form,
-    myResume,
-    vacance
+    CreateResume,
+    someonesResume,
+    CreateVacance
 })
 
-let store = createStore(reducers,applyMiddleware(thunk))
+let store = createStore(reducers, applyMiddleware(thunk))
 
 export type RootType = ReturnType<typeof reducers>
 
