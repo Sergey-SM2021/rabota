ec8def4f6c55f4019d4679de73f98034b8ee52a5
Sergey-SM2021 <84789763+Sergey-SM2021@users.noreply.github.com>
2022-02-17 11:35:40 +0300

    add AC set vacance

--- Frontend/src/components/Vacance/Vacance.tsx
+++ Frontend/src/components/Vacance/Vacance.tsx
@@ -1,16 +1,27 @@
-import { FC } from "react"
-import { useSelector } from "react-redux"
+import { FC, useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
 
 import Style from './Vacance.module.sass'
 import Header from './Header/Header'
 import Content from "./Content/Content"
 import SideBar from "./SideBar/SideBar"
-import * as selectors from '../../redux/selectors' 
+import * as selectors from '../../redux/selectors'
+import { setVacance } from "../../redux/reducers/vacance"
 
 const Vacance: FC = () => {
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(setVacance({
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, tempore vel. Animi distinctio in omnis molestiae eligendi debitis quaerat ullam asperiores facere commodi excepturi, inventore non, eaque optio modi? Minus.",
+            price: 1400,
+            skills: ["ts", "js", "react"],
+            vacance: "Team lead"
+        }
+        ))
+    }, [])
     const vacance = useSelector(selectors.Vacance.getVacance)
     return (<div className={Style.wrapper}>
-        <Header isDistantWork={vacance.isDistantWork} title={vacance.title} price={vacance.price} skills={vacance.skills}/>
+        <Header isDistantWork={true} title={vacance.vacance} price={vacance.price} skills={vacance.skills} />
         <Content />
         <div className={`${Style.newBar} ${Style.item}`}>newBar</div>
         <div className={`${Style.chat} ${Style.item}`}>chat</div>
--- Frontend/src/redux/reducers/vacance.ts
+++ Frontend/src/redux/reducers/vacance.ts
@@ -1,22 +1,46 @@
+import { Dispatch } from "redux"
+import { IVacance } from "../../Models/Models"
+
+enum constants {
+    SETVACANC = "SETVACANC"
+}
+
 type stateType = {
-    vacance: {
-        title: string,
-        price: number,
-        skills: Array<string>,
-        isDistantWork: boolean
-    }
+    vacance: IVacance
 }
 
 const StateInit: stateType = {
     vacance: {
-        price:800,
-        skills:["ts"],
-        title:"Full-Stack senior developer",
-        isDistantWork: true
+        price: 800,
+        skills: ["ts"],
+        description: "description",
+        vacance: ""
     }
 }
 
-export const vacance = (state = StateInit, action: any) => {
+interface ISetVacance {
+    type: constants.SETVACANC
+    payload: IVacance
+}
+
+const SetVacance = (payload:IVacance):ISetVacance => ({
+    payload,
+    type:constants.SETVACANC
+})
+
+type actionType = ISetVacance
+
+export const vacance = (state = StateInit, action: actionType) => {
     let stateCopy = { ...state }
-    return stateCopy
+    switch (action.type) {
+        case constants.SETVACANC:
+            stateCopy.vacance = action.payload
+            return stateCopy
+        default:
+            return stateCopy
+    }
+}
+
+export const setVacance = (vacance: IVacance) => (dispatch: Dispatch) => {
+    dispatch(SetVacance(vacance))
 }
